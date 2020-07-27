2234cd952050092d88a00ab89ab881e60e9f6fd1
Reskyppo <reskybeen@gmail.com>
2020-07-27 23:06:21 +0700

    add responsive page People

--- src/components/People.js
+++ src/components/People.js
@@ -20,32 +20,27 @@ export default class People extends Component {
     }
     render() {
         return (
-            <section className="text-gray-700 body-font overflow-hidden">
-                  <div className="container px-5 mx-auto">
-                    <div className="flex flex-wrap -mx-16 items-stretch">
-                      {this.state.datas.map((data) => (
-                        <div
-                          className="p-12 md:w-1/2 flex flex-col"
-                          key={data.id}
-                        >
-                          <span className="inline-block py-1 px-3 rounded bg-orange-100 text-woodsmoke-500 text-sm w-auto font-medium tracking-widest">
-                            People
-                          </span>
-                          <h2 className="sm:text-3xl text-2xl title-font font-medium text-woodsmoke-500 mt-4 mb-4">
-                            {data.name} ({data.gender})
-                          </h2>
-                          <p className="leading-relaxed mb-8">
-                            age : {data.age},
-                            <br></br>
-                            eye color : {data.eye_color},
-                            <br></br>
-                            hair color : {data.hair_color},
-                          </p>
-                          </div>
-                      ))}
-                    </div>
+        <section className="text-gray-700 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -mx-16 items-stretch">
+              {
+                this.state.datas.map(data=> (
+                  <div
+                          className = "mx-8 md:p-12 flex flex-col md:bg-storm-gray-500 m-2 md:w-2/5"
+                          key       = {data.id}>
+                      <h2 className     = "tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">People</h2>
+                      <h1 className     = "title-font text-xl font-medium text-gray-900 mb-3">{data.name}</h1>
+                      <p  className     = "leading-relaxed mb-5">ages : {data.age}</p>
+                      <p  className     = "leading-relaxed mb-5">gender : {data.gender}</p>
+                      <p  className     = "leading-relaxed mb-5">eye color : {data.eye_color}</p>
+                      <p  className     = "leading-relaxed mb-5">hair color : {data.hair_color}</p>
+                      <div className = ' border-b-2'></div>
                   </div>
-                </section>
+                ))
+              }
+            </div>
+          </div>
+        </section>
         )
     }
 }
