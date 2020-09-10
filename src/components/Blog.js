d0280b485f652433acde9593460527afdb615765
Reskyppo <reskybeen@gmail.com>
2020-09-10 11:17:00 +0700

    finishing content blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,6 +9,7 @@ const Blog = ({ category, link }) => {
 
   console.log(people[0]);
   console.log(people[1]);
+  console.log(people[2]);
 
   useEffect(() => {
     Axios.get("https://ghibliapi.herokuapp.com/people").then(
@@ -30,7 +31,7 @@ const Blog = ({ category, link }) => {
               <span className="tracking-widest font-medium title-font text-gray-900">
                 {category}
               </span>
-              <span className="mt-1 text-gray-500 text-sm">1986</span>
+              <span className="mt-1 text-gray-600 font-semibold text-sm">1986</span>
             </div>
             <div className="md:flex-grow ">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
@@ -69,7 +70,7 @@ const Blog = ({ category, link }) => {
               <span className="tracking-widest font-medium title-font text-gray-900">
                 {category}
               </span>
-              <span className="mt-1 text-gray-500 text-sm">1988</span>
+              <span className="mt-1 text-gray-600 font-semibold text-sm">1988</span>
             </div>
             <div className="md:flex-grow ">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
@@ -107,121 +108,88 @@ const Blog = ({ category, link }) => {
         </div>
       </div>
     </section>
-  ) : null;
-  // <section className="text-gray-700 body-font">
-  //   <div className="container px-5 py-24 mx-auto">
-  //     <div className="flex flex-wrap -mx-4 -my-8">
-  //       <div className="py-8 px-4 lg:w-1/3">
-  //         <div className="h-full flex items-start">
-  //           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
-  //             <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">
-  //               Jul
-  //             </span>
-  //             <span className="font-medium text-xl text-gray-800 title-font">
-  //               18
-  //             </span>
-  //           </div>
-  //           <div className="flex-grow pl-6">
-  //             <h2 className="tracking-widest text-xs title-font font-medium text-blue-500 mb-1">
-  //               CATEGORY
-  //             </h2>
-  //             <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
-  //               The 400 Blows
-  //             </h1>
-  //             <p className="leading-relaxed mb-5">
-  //               Photo booth fam kinfolk cold-pressed sriracha leggings
-  //               jianbing microdosing tousled waistcoat.
-  //             </p>
-  //             <Link className="inline-flex items-center">
-  //               <img
-  //                 alt="blog"
-  //                 src="https://dummyimage.com/103x103"
-  //                 className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
-  //               />
-  //               <span className="flex-grow flex flex-col pl-3">
-  //                 <span className="title-font font-medium text-gray-900">
-  //                   Alper Kamu
-  //                 </span>
-  //               </span>
-  //             </Link>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <div className="py-8 px-4 lg:w-1/3">
-  //         <div className="h-full flex items-start">
-  //           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
-  //             <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">
-  //               Jul
-  //             </span>
-  //             <span className="font-medium text-xl text-gray-800 title-font">
-  //               18
-  //             </span>
-  //           </div>
-  //           <div className="flex-grow pl-6">
-  //             <h2 className="tracking-widest text-xs title-font font-medium text-blue-500 mb-1">
-  //               CATEGORY
-  //             </h2>
-  //             <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
-  //               Shooting Stars
-  //             </h1>
-  //             <p className="leading-relaxed mb-5">
-  //               Photo booth fam kinfolk cold-pressed sriracha leggings
-  //               jianbing microdosing tousled waistcoat.
-  //             </p>
-  //             <Link className="inline-flex items-center">
-  //               <img
-  //                 alt="blog"
-  //                 src="https://dummyimage.com/102x102"
-  //                 className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
-  //               />
-  //               <span className="flex-grow flex flex-col pl-3">
-  //                 <span className="title-font font-medium text-gray-900">
-  //                   Holden Caulfield
-  //                 </span>
-  //               </span>
-  //             </Link>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <div className="py-8 px-4 lg:w-1/3">
-  //         <div className="h-full flex items-start">
-  //           <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
-  //             <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">
-  //               Jul
-  //             </span>
-  //             <span className="font-medium text-xl text-gray-800 title-font">
-  //               18
-  //             </span>
-  //           </div>
-  //           <div className="flex-grow pl-6">
-  //             <h2 className="tracking-widest text-xs title-font font-medium text-blue-500 mb-1">
-  //               CATEGORY
-  //             </h2>
-  //             <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
-  //               Neptune
-  //             </h1>
-  //             <p className="leading-relaxed mb-5">
-  //               Photo booth fam kinfolk cold-pressed sriracha leggings
-  //               jianbing microdosing tousled waistcoat.
-  //             </p>
-  //             <Link className="inline-flex items-center">
-  //               <img
-  //                 alt="blog"
-  //                 src="https://dummyimage.com/101x101"
-  //                 className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
-  //               />
-  //               <span className="flex-grow flex flex-col pl-3">
-  //                 <span className="title-font font-medium text-gray-900">
-  //                   Henry Letham
-  //                 </span>
-  //               </span>
-  //             </Link>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </section>
+  ) : (
+    <section className="text-gray-700 body-font overflow-hidden">
+      <div className="container px-5 py-8 mx-auto">
+        <div className="-my-8">
+          <div className="py-8 flex flex-wrap md:flex-no-wrap">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">
+                {category}
+              </span>
+              <span className="mt-1 text-gray-600 font-semibold text-sm">13 year old</span>
+            </div>
+            <div className="md:flex-grow ">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                Pazu
+              </h2>
+              <p className="leading-relaxed">
+                Pazu is the protagonist of Castle in the Sky. He is Sheeta's
+                best friend. Pazu has made proving that Laputa exists his
+                mission in life after his father found the place and was
+                believed to be a liar for his discovery.
+              </p>
+              <Link
+                className="text-blue-500 inline-flex items-center mt-4"
+                to={`/${link}`}
+              >
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </Link>
+            </div>
+          </div>
+          <div className="py-8  border-t-2 border-gray-200 flex-wrap md:flex-no-wrap hidden sm:flex">
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <span className="tracking-widest font-medium title-font text-gray-900">
+                {category}
+              </span>
+              <span className="mt-1 text-gray-600 font-semibold text-sm">13 year old</span>
+            </div>
+            <div className="md:flex-grow ">
+              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                Lusheeta Toel Ul Laputa
+              </h2>
+              <p className="leading-relaxed">
+                Sheeta, whose full name is Lusheeta Toel Ul Laputa ("Toel"
+                meaning "true" and "Ul" meaning "ruler" in Laputian), is a
+                13-year-old orphaned farm girl. Her glowing stone keeps Laputa
+                alive.
+              </p>
+              <Link
+                className="text-blue-500 inline-flex items-center mt-4"
+                to={`/${link}`}
+              >
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
